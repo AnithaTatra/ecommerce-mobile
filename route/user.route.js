65fdbb0ef82fcfe054d95bd1c7ae4f72343dcd13
AnithaTatra <102857170+AnithaTatra@users.noreply.github.com>
2022-05-07 22:19:28 +0530

    commited

--- route/user.route.js
+++ route/user.route.js
@@ -2,16 +2,18 @@
 const router = require("express").Router()
 //const bcrypt = require("bcrypt")
 const userSchema = require('../model/user.model');
-const curdoprations=require('../model/crud.oprations')
+const curdoperations=require('../model/crud.operations')
 var constants = require('../constants/constants.status');
 const{validation} = require('../model/fieldsValidation')
+const mailSending = require('../middleware/email')
+//const {authVerification} = require('../middleware/auth');
 
 router.post('/signUp',async(req,res)=>{
     try{
         const username = req.body.username;
         const email  = req.body.email;
         if(username){
-            let userData = await validation.DataValidation.userSchema.findOne({username:username}).exec();
+            let userData = await userSchema.findOne({username:username}).exec();
             if(userData){
                  res.json({status:constants.USER_STATUS.FAILURE_STATUS, message:constants.USER_STATUS.USER_NAME_EXISTS})
             }else{
@@ -22,20 +24,31 @@ router.post('/signUp',async(req,res)=>{
                 }       
             }
         }  
-        var result=curdoprations.signupSave(req,res)
+        var result=curdoperations.signupSave(req,res)
         if(result){
           res.status(200).json({status:constants.USER_STATUS.SUCCESS_STATUS,message:constants.USER_STATUS.CREATED_SUCCESS})            
+        }
+        const toMail = email;
+        console.log("toMail..."+toMail);
+        const compose={
+            from:"pinkyangelqueen123@gmail.com",
+            to:toMail,
+            fileName:'activeemail.ejs',
+            details:{
+                mail:toMail
+            }
+        }
+        let mailData = await mailSending.mailSending(compose)
     }
-}
     }catch(error){
         console.log(error.message)
        res.status(400).json({status:constants.USER_STATUS.FAILURE_STATUS,message:constants.USER_STATUS.NO_DATA})  
     }
 });
 
- router.post('/userLogin',authVerify,async(req,res)=>{
+ router.post('/userLogin',async(req,res)=>{
      try{
-          curdoprations.loginPage(req,res)
+          curdoperations.loginPage(req,res)
      }catch(error){
           res.status(400).json({status:constants.USER_STATUS.FAILURE_STATUS,message:constants.USER_STATUS.NO_DATA})
      }
@@ -43,11 +56,30 @@ router.post('/signUp',async(req,res)=>{
 
  router.put('/userLogout/:uuid', async(req,res)=>{
      try{
-         curdoprations.logoutpage(req,res)
+         curdoperations.logoutpage(req,res)
      }catch(error){
           res.status(400).json({status:constants.USER_STATUS.FAILURE_STATUS, message:constants.USER_STATUS.NO_DATA})
      }
  });
 
- 
+ router.get('/accountVerify/:accountVerifyStatus/:mail',async(req,res)=>{
+     try{
+        var accountVerifyStatus=req.params.accountVerifyStatus;
+        var mail=req.params.mail;
+        console.log("Mail.....",mail)
+        console.log("accountVerifyStatus..",accountVerifyStatus);
+        const compose={
+            from:"pinkyangelqueen123@gmail.com",
+            to:mail,
+            fileName:'welcomePage.ejs',
+    
+        }
+        let mailData = await mailSending.mailSending(compose)
+        await userSchema.findOneAndUpdate({email:req.params.mail},{accountVerifyStatus:req.params.accountVerifyStatus},{new:true}).exec()
+        res.status(200).json({status:constants.USER_STATUS.SUCCESS_STATUS,message:constants.USER_STATUS.ACCOUNT_STATUS})            
+
+     }catch(error){
+         res.status(400).json({status:constants.USER_STATUS.FAILURE_STATUS,message:constants.USER_STATUS.NO_DATA})
+     }
+ })
 module.exports = router;
